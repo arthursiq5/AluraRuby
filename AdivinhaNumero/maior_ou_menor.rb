79071256cf2e8bd90dfa18971e9f5ccec918a5fa
Arthur Siqueira <arthur.siq5@gmail.com>
2020-04-06 23:29:08 -0300

    pegando dados da entrada do teclado e apresentando no terminal

--- AdivinhaNumero/maior_ou_menor.rb
+++ AdivinhaNumero/maior_ou_menor.rb
@@ -1,1 +1,13 @@
 puts "Bem vindo ao jogo da adivinhação"
+puts "Qual é o seu nome?"
+
+# pegando entrada do teclado
+nome = gets
+
+puts
+puts
+puts
+puts
+puts
+puts "Começaremos o jogo pra você, "
+puts nome
