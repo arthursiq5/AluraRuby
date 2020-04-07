f21cab2f8c06eeed1cd50dcbf9eaba436c1ecd82
Arthur Siqueira <arthur.siq5@gmail.com>
2020-04-07 01:10:20 -0300

    mais alguns elementos decorativos no programa

--- AdivinhaNumero/maior_ou_menor.rb
+++ AdivinhaNumero/maior_ou_menor.rb
@@ -10,3 +10,20 @@ puts
 puts
 puts
 puts "Começaremos o jogo pra você, " + nome
+
+puts "Escolhendo um número entre 0 e 200..."
+
+numero_secreto = 175
+
+puts "Escolhido... Que tal adivinhar hoje nosso número secreto?"
+
+puts
+puts
+puts
+puts
+puts "Tentativa 1"
+puts "Entre com o número: "
+
+chute = gets
+
+puts "Será que acertou? Você chutou " + chute
